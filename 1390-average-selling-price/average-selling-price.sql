f95a062d46f15c694d0c85382f0149e1043c0c62
VINAYAK <93870082+vinayak-varshney@users.noreply.github.com>
2024-05-19 19:50:28 +0530

    Time: 1511 ms (51.29%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,20 +1,8 @@
-with result as (
-select 
+SELECT 
     p.product_id,
-    u.purchase_date,
-    coalesce(u.units, 0) as units,
-    p.price * coalesce(u.units, 0) as total_amt,
-    sum(coalesce(u.units, 0)) over (partition by p.product_id) as total_quant
-from 
-    Prices p
-    left join UnitsSold u on p.product_id = u.product_id 
-    and u.purchase_date >= start_date 
-    and u.purchase_date <= end_date
-)
-select 
-    product_id, 
-    round(coalesce(sum(total_amt) / nullif(total_quant, 0), 0), 2) as average_price
-from 
-    result
-group by 
-    product_id;
+    IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM prices p
+LEFT JOIN unitssold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN start_date AND end_date
+GROUP BY product_id

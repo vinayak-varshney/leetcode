be8ede304854eafd9492721147bb5406e31eff27
VINAYAK <93870082+vinayak-varshney@users.noreply.github.com>
2024-05-19 15:38:42 +0530

    Time: 1392 ms (41.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,9 @@
+SELECT 
+    s.user_id,
+    CASE 
+        WHEN COUNT(c.action) = 0 THEN 0.00
+        ELSE ROUND(CAST(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) as float) / COUNT(c.action), 2)
+    END AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;

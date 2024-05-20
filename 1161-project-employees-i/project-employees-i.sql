cc4b5574399fbf8e9a708f1c16eb48a735989da4
VINAYAK <93870082+vinayak-varshney@users.noreply.github.com>
2024-05-20 10:07:06 +0530

    Time: 1228 ms (30.51%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,4 @@
-select p.project_id, round(sum(e.experience_years)/count(p.project_id),2) as average_years
-from project p join employee e
+select p.project_id, round(avg(e.experience_years),2) as average_years
+from project p left join employee e
 on p.employee_id=e.employee_id
 group by p.project_id
